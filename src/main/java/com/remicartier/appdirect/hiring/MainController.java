6365cab7687fa9ffb2f395bc49e87fe82a2d4e4b
Rémi Cartier <remi.cartier@gmail.com>
2015-04-10 19:37:46 -0400

    added logging via logback

--- src/main/java/com/remicartier/appdirect/hiring/MainController.java
+++ src/main/java/com/remicartier/appdirect/hiring/MainController.java
@@ -1,4 +1,6 @@
 package com.remicartier.appdirect.hiring;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
  
 @Controller
 public class MainController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(@ModelAttribute("model") ModelMap model) {
+        LOGGER.info("GET /");
         model.addAttribute("userList", null);
         return "index";
     }
